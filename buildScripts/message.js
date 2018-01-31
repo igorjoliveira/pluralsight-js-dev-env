acb09f9df20baa97f37f48d948446b84403fa85e
Igor José de Oliveira <igorjoliveira@outlook.com>
2018-01-31 11:53:50 -0200

    update file to disable esLint

--- buildScripts/message.js
+++ buildScripts/message.js
@@ -3,4 +3,4 @@
 /*sintax Babel*/
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
